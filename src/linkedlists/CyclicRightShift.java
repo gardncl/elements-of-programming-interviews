20f3b65274b13cd1942d49e1b9bf3de0694fb74a
gardncl <gardncl@gmail.com>
2017-04-14 06:51:43 -0400

    cycle right shift tests

---
+++ src/linkedlists/CyclicRightShift.java
@@ -0,0 +1,32 @@
+package linkedlists;
+
+import datastructures.ListNode;
+
+public class CyclicRightShift {
+
+    /*
+    Write a program that takes as input a single linked list
+    and a non-negative integer k, and returns the list
+    cyclically shifted to the right by k.
+     */
+
+    //1 2 3 4 5
+    //3 4 5 1 2
+
+    public static ListNode<Integer> shift(int k, ListNode<Integer> list) {
+        ListNode<Integer> cut = list;
+        ListNode<Integer> glue = list;
+        if (k == 0)
+            return list;
+        int length = 1;
+        while (glue.next != null) {
+            glue = glue.next;
+            ++length;
+        }
+        glue.next = list;
+        System.out.println("Length: "+length+" k: "+k);
+        list = cut.get(length - k).next;
+        cut.get(length - k).next = null;
+        return list;
+    }
+}
---
+++ test/linkedlists/CyclicRightShiftTest.java
@@ -0,0 +1,54 @@
+package linkedlists;
+
+import datastructures.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CyclicRightShiftTest {
+
+    private ListNode<Integer> expected;
+    private ListNode<Integer> input;
+    private int k;
+
+    @Test
+    void shift1() {
+        expected = LinkedListTestUtil.createLinkedList(1, 2, 3, 4, 5);
+        input = LinkedListTestUtil.createLinkedList(1, 2, 3, 4, 5);
+        k = 0;
+
+        test(expected, input, k);
+    }
+
+    @Test
+    void shift2() {
+        expected = LinkedListTestUtil.createLinkedList(1, 2, 3, 4, 5);
+        input = LinkedListTestUtil.createLinkedList(2, 3, 4, 5, 1);
+        k = 4;
+
+        test(expected, input, k);
+    }
+
+    @Test
+    void shift3() {
+        expected = LinkedListTestUtil.createLinkedList(1, 2, 3, 4, 5);
+        input = LinkedListTestUtil.createLinkedList(3, 4, 5, 1, 2);
+        k = 3;
+
+        test(expected, input, k);
+    }
+
+    @Test
+    void shift4() {
+        expected = LinkedListTestUtil.createLinkedList(1, 2);
+        input = LinkedListTestUtil.createLinkedList(2, 1);
+        k = 1;
+
+        test(expected, input, k);
+    }
+
+    private void test(ListNode<Integer> expected, ListNode<Integer> input, int k) {
+        LinkedListTestUtil.assertSameList(expected, CyclicRightShift.shift(k, input));
+    }
+
+}
