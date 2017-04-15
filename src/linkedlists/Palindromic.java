65d2279a2573b6112573705843617e86b5d74bbe
gardncl <gardncl@gmail.com>
2017-04-15 15:37:49 -0400

    linked list palindrome

--- src/linkedlists/Palindromic.java
+++ src/linkedlists/Palindromic.java
@@ -9,8 +9,39 @@ public class Palindromic {
      */
 
     public static boolean isPalindromic(ListNode<Integer> list) {
+        if (list.next == null)
+            return true;
 
-        return false;
+        ListNode<Integer> slow = new ListNode<>(0, list);
+        ListNode<Integer> fast = new ListNode<>(0, list);
+        ListNode<Integer> numbers = null;
+
+        boolean add = true;
+
+        //BUILD LIST TO STORE THE VARIABLES
+        while (fast.next.next != null) {
+            fast.next = fast.next.next;
+            if (add) {
+                numbers = new ListNode<>(slow.next.data, numbers);
+                slow.next = slow.next.next;
+            }
+            add = !add;
+        }
+
+        //THERE ARE AN ODD NUMBER OF ELEMENTS
+        if (add)
+            slow = slow.next;
+
+        //ITERATE THROUGH THE LIST
+        while (slow.next != null) {
+            if (slow.next.data != numbers.data) {
+                return false;
+            }
+            slow.next = slow.next.next;
+            numbers = numbers.next;
+        }
+
+        return true;
     }
 
 }
