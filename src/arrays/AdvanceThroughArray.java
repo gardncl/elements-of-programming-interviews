5b5004dc18ce2efcd3c9e80cbf0a3cbe2c4b44cb
gardncl <gardncl@gmail.com>
2017-04-15 11:47:27 -0400

    Removed solution from advance array

--- src/arrays/AdvanceThroughArray.java
+++ src/arrays/AdvanceThroughArray.java
@@ -12,19 +12,7 @@ public class AdvanceThroughArray {
     */
 
     public static boolean arrayAdvance(List<Integer> A) {
-        int steps = 0, count = 0, length = A.size() - 1;
-        while (steps >= 0) {
-            //UPDATE TOTAL NUMBER OF STEPS IF GREATER THAN STORED
-            if (A.get(count) > steps) {
-                steps = A.get(count);
-            }
-            steps--;
-            count++;
 
-            //IF YOU CAN REACH THE LAST INDEX THEN RETURN TRUE
-            if (count + steps >= length)
-                return true;
-        }
         return false;
     }
 }
