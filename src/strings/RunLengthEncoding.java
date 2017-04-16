d5caf3063a4b0bd5d5f070fedb73c76158e48008
gardncl <gardncl@gmail.com>
2017-04-16 16:05:51 -0400

    encode and decode

--- src/strings/RunLengthEncoding.java
+++ src/strings/RunLengthEncoding.java
@@ -12,13 +12,43 @@ public class RunLengthEncoding {
      */
 
     public static String encode(String s) {
+        StringBuilder code = new StringBuilder();
 
-        return "";
+        int n = 0;
+        char current = ' ';
+        for (char c : s.toCharArray()) {
+            if (c == current) {
+                n++;
+            } else if (n > 0){
+                code.append(n);
+                code.append(current);
+                current = c;
+                n = 1;
+            } else {
+                current = c;
+                n = 1;
+            }
+        }
+        if (n > 0){
+            code.append(n);
+            code.append(current);
+        }
+
+        return code.toString();
     }
 
     public static String decode(String s) {
-
-        return "";
+        StringBuilder message = new StringBuilder();
+        int n = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c))
+                n = c - '0';
+            else {
+                while (n-- > 0)
+                    message.append(c);
+            }
+        }
+        return message.toString();
     }
 
 }
