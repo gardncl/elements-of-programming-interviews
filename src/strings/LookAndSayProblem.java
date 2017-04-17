8979da1a4054ece374531cee5ae99df664916fbf
gardncl <gardncl@gmail.com>
2017-04-17 18:43:50 -0400

    look and say

--- src/strings/LookAndSayProblem.java
+++ src/strings/LookAndSayProblem.java
@@ -1,5 +1,9 @@
 package strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LookAndSayProblem {
 
     /*
@@ -11,8 +15,35 @@ public class LookAndSayProblem {
      */
 
     public static String lookAndSay(int n) {
+        List<Integer> returnValue = Arrays.asList(1);
+        int count = 0, current = -1;
+        for (int i = 1; i < n; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (Integer val : returnValue) {
+                if (val != current) {
+                    if (count > 0){
+                        temp.add(count);
+                        temp.add(current);
+                    }
+                    current = val;
+                    count = 1;
+                } else {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                temp.add(count);
+                temp.add(current);
+            }
+            count = 0;
+            returnValue = new ArrayList<>(temp);
+        }
 
-        return "";
+        StringBuilder num = new StringBuilder();
+        for (Integer val : returnValue) {
+            num.append(val);
+        }
+        return num.toString();
     }
 
 }
