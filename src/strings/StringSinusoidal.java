7a4994d356751cecb2efb0af37b648b0cbfc6da5
gardncl <gardncl@gmail.com>
2017-04-16 21:11:36 -0400

    StringSinusoidal

--- src/strings/StringSinusoidal.java
+++ src/strings/StringSinusoidal.java
@@ -6,12 +6,18 @@ public class StringSinusoidal {
     7.11
 
     Write a program which takes as input a string s
-    and returns the snakestring of s.
+    and returns the snake string of s.
      */
 
     public static String snakeString(String s) {
-
-        return "";
+        StringBuilder snake = new StringBuilder();
+        for (int i = 1; i < s.length(); i+=4)
+            snake.append(s.charAt(i));
+        for (int i = 0; i < s.length(); i+=2)
+            snake.append(s.charAt(i));
+        for (int i = 3; i < s.length(); i+=4)
+            snake.append(s.charAt(i));
+        return snake.toString();
     }
 
 }
