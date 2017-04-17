33c8f7f748ce3ea7755e68c5a090894e63351457
gardncl <gardncl@gmail.com>
2017-04-16 21:15:20 -0400

    reverse words in a sentence

--- src/strings/ReverseWordsInASentence.java
+++ src/strings/ReverseWordsInASentence.java
@@ -9,7 +9,11 @@ public class ReverseWordsInASentence {
      */
 
     public static String reverseWordsInASentence(String input) {
-
-        return "";
+        StringBuilder sentence = new StringBuilder();
+        String[] words = input.split(" ");
+        int i = words.length;
+        while (i-- > 0)
+            sentence.append(words[i]+" ");
+        return sentence.toString().trim();
     }
 }
