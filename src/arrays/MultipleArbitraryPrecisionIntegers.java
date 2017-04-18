313dbddff8dda72336bb48e3edcede4f6aaec4eb
gardncl <gardncl@gmail.com>
2017-04-17 22:03:50 -0400

    new test cases and permute elements done

---
+++ src/arrays/MultipleArbitraryPrecisionIntegers.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipleArbitraryPrecisionIntegers {
+
+    /*
+    6.3
+
+    Write a program that takes two arrays representing integers,
+    and returns an integer representing their product.
+     */
+
+    public static List<Integer> multiply(List<Integer> a, List<Integer> b) {
+
+        return new ArrayList<>();
+    }
+}
--- src/arrays/PermuteElements.java
+++ src/arrays/PermuteElements.java
@@ -1,8 +1,23 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermuteElements {
 
     /*
+    6.9
+
     Given an array A of n elements and a permutation P, apply P to A.
      */
+
+    public static void applyPermutation(List<Integer> perm, List<Integer> a) {
+        List<Integer> sorted = new ArrayList<>();
+        for (int i = 0; i < perm.size(); i++) {
+            sorted.add(a.get(perm.get(i)));
+        }
+        for (int i = 0; i < sorted.size(); i++) {
+            a.set(i, sorted.get(i));
+        }
+    }
 }
