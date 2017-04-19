95f7fb8383414bb05ed5119ecd9c138616579960
gardncl <gardncl@gmail.com>
2017-04-19 18:04:17 -0400

    compute parity

---
+++ src/primatives/ComputeParity.java
@@ -0,0 +1,13 @@
+package primatives;
+
+public class ComputeParity {
+
+    /*
+    Compute the parity of a 64-bit word.
+     */
+
+    public static short parity(long n) {
+
+        return 0;
+    }
+}
