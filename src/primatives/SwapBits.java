b2e25331077f40d70364e5dad9778da6959f034f
gardncl <gardncl@gmail.com>
2017-04-20 19:57:30 -0400

    swap bits done

--- src/primatives/SwapBits.java
+++ src/primatives/SwapBits.java
@@ -13,7 +13,10 @@ public class SwapBits {
      */
 
     public static long swapBits(long x, int i, int j) {
-
-        return 0L;
+        if (((x >>> i) & 1) != ((x >>> j) & 1)) {
+            final long MASK = (1 << i) | (1 << j);
+            x ^= MASK;
+        }
+        return x;
     }
 }
