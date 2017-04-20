3d05a86ea157f29c9666afba8a49fa56403f6cb7
gardncl <gardncl@gmail.com>
2017-04-20 19:12:09 -0400

    parity with xors

--- src/primatives/ComputeParity.java
+++ src/primatives/ComputeParity.java
@@ -6,14 +6,14 @@ public class ComputeParity {
     Compute the parity of a 64-bit word.
      */
 
-    private static short[] CACHE = new short[]{0,1,1,0};
-    private static int MASK = 3;
-    private static int WORD_SIZE = 16;
 
     public static short parity(long n) {
-        return(short)( CACHE[(int)(n>>>(3*WORD_SIZE) & MASK)]
-                ^ CACHE[(int)(n>>>(2*WORD_SIZE) & MASK)]
-                ^ CACHE[(int)(n>>>WORD_SIZE & MASK)]
-                ^ CACHE[(int)(n & MASK)]);
+        n ^= n >>> 32;
+        n ^= n >>> 16;
+        n ^= n >>> 8;
+        n ^= n >>> 4;
+        n ^= n >>> 2;
+        n ^= n >>> 1;
+        return (short)(n & 0x1);
     }
 }
