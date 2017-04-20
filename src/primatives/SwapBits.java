ec4ba196232beeed1cf176928bf1fefdd143f1c1
gardncl <gardncl@gmail.com>
2017-04-20 19:29:41 -0400

    test cases and stub for swap bits

---
+++ src/primatives/SwapBits.java
@@ -0,0 +1,19 @@
+package primatives;
+
+public class SwapBits {
+
+    /*
+    5.2
+
+    A 64-bit integer can be viewed as an array of 64 bits,
+    with the bit at index 0 corresponding to the least
+    significant bit (LSB), and the bit at index 63 corresponding
+    to the most significant bit (MSG). Implement code that
+    takes as input a 64-bit integer and swaps the bits at indices i and j.
+     */
+
+    public static long swapBits(long x, int i, int j) {
+
+        return 0L;
+    }
+}
