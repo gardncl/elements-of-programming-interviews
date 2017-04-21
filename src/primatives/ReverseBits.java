662ec6744aa1fac64908fcb6ce559eb35f6bdcf0
gardncl <gardncl@gmail.com>
2017-04-21 17:33:04 -0400

    reverse digits done

--- src/primatives/ReverseBits.java
+++ src/primatives/ReverseBits.java
@@ -12,10 +12,7 @@ public class ReverseBits {
 
     public static long reverse(long x) {
         long returnValue = 0;
-        for (int i = 0; i < 64; i++) {
-            returnValue ^= (x >>> (63 - i)) ^ 1;
-            returnValue <<= 1;
-        }
+
         return returnValue;
     }
 }
--- src/primatives/ReverseDigits.java
+++ src/primatives/ReverseDigits.java
@@ -11,7 +11,17 @@ public class ReverseDigits {
      */
 
     public static long reverse(int x) {
-
-        return 0;
+        long returnValue = 0;
+        long neg = 1;
+        if (x < 0) {
+            neg = -1;
+            x *= neg;
+        }
+        while (x !=0 ) {
+            returnValue *= 10;
+            returnValue += x % 10;
+            x /= 10;
+        }
+        return returnValue * neg;
     }
 }
