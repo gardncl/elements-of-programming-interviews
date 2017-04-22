0e88d742b2c1a58c60e67ace0beef50f5a0d27f4
gardncl <gardncl@gmail.com>
2017-04-22 07:50:38 -0400

    compute power test cases done

--- test/primatives/ComputePowerTest.java
+++ test/primatives/ComputePowerTest.java
@@ -7,21 +7,49 @@ import static primatives.ComputePower.power;
 
 class ComputePowerTest {
 
+    private final double EPSILON = .1;
     private double expected;
     private double x;
     private int y;
 
     @Test
     void power1() {
-        expected = 0;
-        x = 0;
-        y = 0;
+        expected = 4;
+        x = 2;
+        y = 2;
+
+        test(expected, x, y);
+    }
+
+    @Test
+    void power2() {
+        expected = 783.11;
+        x = 2.3;
+        y = 8;
+
+        test(expected, x, y);
+    }
+
+    @Test
+    void power4() {
+        expected = 9;
+        x = -3;
+        y = 2;
+
+        test(expected, x, y);
+    }
+
+    @Test
+    void power5() {
+        expected = -27;
+        x = -3;
+        y = 3;
 
         test(expected, x, y);
     }
 
     private void test(double expected, double x, int y) {
-        assertEquals(expected, power(x,y));
+        assertEquals(expected,power(x,y),EPSILON);
     }
 
 }
