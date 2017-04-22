a555f2b20496d87ac73381acad8a92e279bff696
gardncl <gardncl@gmail.com>
2017-04-22 14:42:48 -0400

    closest int done

--- src/primatives/FindClosestInteger.java
+++ src/primatives/FindClosestInteger.java
@@ -12,7 +12,12 @@ public class FindClosestInteger {
      */
 
     public static long closestIntSameBitCount(long x) {
-
-        return 0;
+        for (int i = 0; i < 62; i++) {
+            if (((x >>> i) & 1) != ((x >>> i + 1) & 1)) {
+                long MASK = ((1 << i) | (1 << i + 1));
+                return x ^ MASK;
+            }
+        }
+        return x;
     }
 }
