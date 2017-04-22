c47f69e8c9130b45501558e3d598d52d0def1d7e
gardncl <gardncl@gmail.com>
2017-04-22 08:42:45 -0400

    multiply no aritmetic ops

--- test/primatives/MultiplicationNoArithmeticOperatorsTest.java
+++ test/primatives/MultiplicationNoArithmeticOperatorsTest.java
@@ -13,9 +13,27 @@ class MultiplicationNoArithmeticOperatorsTest {
 
     @Test
     void multiply1() {
+        expected = 4;
+        x = 2;
+        y = 2;
+
+        test(expected,x,y);
+    }
+
+    @Test
+    void multiply2() {
         expected = 0;
-        x = 0;
-        y = 0;
+        x = 2;
+        y = 2;
+
+        test(expected,x,y);
+    }
+
+    @Test
+    void multiply3() {
+        expected = -27;
+        x = 9;
+        y = -3;
 
         test(expected,x,y);
     }
