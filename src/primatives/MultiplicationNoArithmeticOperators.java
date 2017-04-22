0c1aa0669c84c2ffc294628cd03f2e75594c936d
gardncl <gardncl@gmail.com>
2017-04-22 17:12:39 -0400

    multiply primative

--- src/primatives/MultiplicationNoArithmeticOperators.java
+++ src/primatives/MultiplicationNoArithmeticOperators.java
@@ -16,7 +16,32 @@ public class MultiplicationNoArithmeticOperators {
      */
 
     public static long multiply(long x, long y) {
+        long result = 0;
+        while (x != 0) {
+            if ((x & 1) == 1) {
+                result = add(result, y);
+            }
+            x >>>= 1;
+            y <<= 1;
+        }
+        return result;
+    }
 
-        return 0;
+    private static long add(long a, long b) {
+        long result = 0, carryin = 0, k = 1, tempA = a, tempB = b;
+        while (tempA != 0 || tempB != 0) {
+            //DO THEY MATCH?
+            long ak = a & k, bk = b & k;
+            //WILL THERE BE A CARRY BIT?
+            long carryout = (ak & bk) | (ak & carryin) | (bk & carryin);
+
+            result |= (ak ^ bk ^ carryin);
+            carryin = carryout << 1;
+            k <<= 1;
+            tempA >>>= 1;
+            tempB >>>= 1;
+        }
+
+        return result | carryin;
     }
 }
