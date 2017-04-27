ad0fdebd11f73df047cbb0e1b73f99f003fde410
gardncl <gardncl@gmail.com>
2017-04-27 19:15:56 -0400

    10.6 & 10.7

---
+++ binarytrees/src/test/java/FindRootToLeafSumTest.java
@@ -0,0 +1,51 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindRootToLeafSumTest {
+
+    private boolean expected;
+    private BinaryTree<Integer> tree;
+    private int targetSum;
+
+    @Test
+    public void hasPathSum1() throws Exception {
+        expected = true;
+        tree = BinaryTreeUtil.getFullTree();
+        targetSum = 10;
+
+        test(expected, tree, targetSum);
+    }
+
+    @Test
+    public void hasPathSum2() throws Exception {
+        expected = false;
+        tree = BinaryTreeUtil.getFullTree();
+        targetSum = 5;
+
+        test(expected, tree, targetSum);
+    }
+
+    @Test
+    public void hasPathSum3() throws Exception {
+        expected = true;
+        tree = BinaryTreeUtil.getFigureTenDotOne();
+        targetSum = 519;
+
+        test(expected, tree, targetSum);
+    }
+
+    @Test
+    public void hasPathSum4() throws Exception {
+        expected = false;
+        tree = BinaryTreeUtil.getFigureTenDotOne();
+        targetSum = 591;
+
+        test(expected, tree, targetSum);
+    }
+
+    private void test(boolean expected, BinaryTree<Integer> tree, int targetSum) {
+        assertEquals(expected, FindRootToLeafSum.hasPathSum(tree, targetSum));
+    }
+
+}
---
+++ binarytrees/src/test/java/InorderIterativeTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class InorderIterativeTest {
+
+    private List<Integer> expected;
+    private BinaryTree<Integer> tree;
+
+    @Test
+    public void BSTInOrder1() throws Exception {
+        expected = Arrays.asList(2,1,0);
+        tree = BinaryTreeUtil.getOddTree();
+
+        test(expected, tree);
+    }
+
+    @Test
+    public void BSTInOrder2() throws Exception {
+        expected = Arrays.asList(2,1,3,0,5,4,6);
+        tree = BinaryTreeUtil.getFullTree();
+
+        test(expected, tree);
+    }
+
+    @Test
+    public void BSTInOrder3() throws Exception {
+        expected = Arrays.asList(28,271,0,6,561,17,3,314,2,401,614,1,257,6,271,28);
+        tree = BinaryTreeUtil.getFigureTenDotOne();
+
+        test(expected, tree);
+    }
+
+    private void test(List<Integer> expected, BinaryTree<Integer> tree) {
+        assertEquals(expected, InorderIterative.BSTInOrder(tree));
+    }
+
+}
