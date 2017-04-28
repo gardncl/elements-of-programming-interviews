1d6880a622dd98c5230cc3ba0ca6ae89e15ff532
gardncl <gardncl@gmail.com>
2017-04-27 20:45:20 -0400

    10.15

--- binarytrees/src/test/java/ComputeExteriorTest.java
+++ binarytrees/src/test/java/ComputeExteriorTest.java
@@ -6,16 +6,17 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class TreeToLinkedListTest {
+public class ComputeExteriorTest {
 
     private List<BinaryTree<Integer>> expected;
     private BinaryTree<Integer> tree;
 
     @Test
-    public void createListOfLeaves1() throws Exception {
+    public void exteriorBinaryTree1() throws Exception {
         tree = BinaryTreeUtil.getEvenTree();
         expected = new LinkedList<>(
                 Arrays.asList(
+                        tree,
                         tree.left,
                         tree.right)
         );
@@ -24,37 +25,44 @@ public class TreeToLinkedListTest {
     }
 
     @Test
-    public void createListOfLeaves2() throws Exception {
+    public void exteriorBinaryTree2() throws Exception {
         tree = BinaryTreeUtil.getFullTree();
         expected = new LinkedList<>(
                 Arrays.asList(
-                        tree.left.left,
+                        tree,
+                        tree.left,
                         tree.left.right,
                         tree.right.left,
-                        tree.right.right)
+                        tree.right.right,
+                        tree.right)
         );
 
         test(expected, tree);
     }
 
-    @Test
-    public void createListOfLeaves3() throws Exception {
+    @Test//a b c d e h m n p o i
+    public void exteriorBinaryTree3() throws Exception {
         tree = BinaryTreeUtil.getFigureTenDotOne();
         expected = new LinkedList<>(
                 Arrays.asList(
+                        tree,
+                        tree.left,
+                        tree.left.left,
                         tree.left.left.left,
                         tree.left.left.right,
                         tree.left.right.right.left,
                         tree.right.left.right.left.right,
                         tree.right.left.right.right,
-                        tree.right.right.right)
+                        tree.right.right.right,
+                        tree.right.right,
+                        tree.right)
         );
 
         test(expected, tree);
     }
 
     private void test(List<BinaryTree<Integer>> expected, BinaryTree<Integer> tree) {
-        assertEquals(expected,TreeToLinkedList.createListOfLeaves(tree));
+        assertEquals(expected,ComputeExterior.exteriorBinaryTree(tree));
     }
 
 }
