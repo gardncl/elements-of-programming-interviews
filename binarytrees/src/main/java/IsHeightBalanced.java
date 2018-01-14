5b251d9841f0164f1dc8d4b4e8ace8a80bc693a7
stengali <stengali@paypal.com>
2018-01-14 12:35:24 -0800

    isHeightBalanced

--- binarytrees/src/main/java/IsHeightBalanced.java
+++ binarytrees/src/main/java/IsHeightBalanced.java
@@ -1,11 +1,37 @@
+class BalanceStatus {
+    boolean balanced;
+    int height;
+    BalanceStatus(boolean balanced, int height) {
+        this.balanced = balanced;
+        this.height = height;
+    }
+}
+
 public class IsHeightBalanced {
 
     /*
     10.1
     */
-
     public static boolean isBalanced(BinaryTree<Integer> tree) {
+        if(tree == null)
+            return true;
+        return helper(tree).balanced;
+    }
 
-        return false;
+    private static BalanceStatus helper(BinaryTree<Integer> tree) {
+        if(tree == null)
+            return new BalanceStatus(true, -1);
+        BalanceStatus left = helper(tree.left);
+        if(!left.balanced) {
+            return left;
+        }
+
+        BalanceStatus right = helper(tree.right);
+        if(!right.balanced)
+            return right;
+
+        boolean isBalanced = Math.abs(left.height - right.height) <= 1;
+        int height = Math.max(left.height, right.height) + 1;
+        return new BalanceStatus(isBalanced, height);
     }
 }
