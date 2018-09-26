f4e4a268856f68832ff86c2fe5408f0bb54f4e28
varunu28 <varun.u28@gmail.com>
2018-09-26 13:56:17 -0700

    Corrected the test for IncrementArbitaryPrecisionInteger

--- arrays/src/test/java/IncrementArbitraryPrecisionIntegerTest.java
+++ arrays/src/test/java/IncrementArbitraryPrecisionIntegerTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,22 +13,23 @@ public class IncrementArbitraryPrecisionIntegerTest {
 
     @Test
     public void incrementInteger1() {
-        input = Arrays.asList(1, 2, 9);
-        incremented = Arrays.asList(1, 3, 0);
+        input = getArrayList(new int[]{1, 2, 9});
+        incremented = getArrayList(new int[]{1, 3, 0});
+
         test(input,incremented);
     }
 
     @Test
     public void incrementInteger2() {
-        input = Arrays.asList(9,9,9,9,9,9,9);
-        incremented = Arrays.asList(0,0,0,0,0,0,0);
+        input = getArrayList(new int[]{9,9,9,9,9,9,9});
+        incremented = getArrayList(new int[]{1,0,0,0,0,0,0,0});
         test(input,incremented);
     }
 
     @Test
     public void incrementInteger3() {
-        input = Arrays.asList(8,9,9,9,9,9,9);
-        incremented = Arrays.asList(9,0,0,0,0,0,0);
+        input = getArrayList(new int[]{8,9,9,9,9,9,9});
+        incremented = getArrayList(new int[]{9,0,0,0,0,0,0});
         test(input,incremented);
     }
 
@@ -36,5 +38,10 @@ public class IncrementArbitraryPrecisionIntegerTest {
         assertEquals(incremented, input);
     }
 
-
+    private List<Integer> getArrayList(int[] arr) {
+        return Arrays.
+                stream(arr).
+                boxed().
+                collect(Collectors.toList());
+    }
 }
